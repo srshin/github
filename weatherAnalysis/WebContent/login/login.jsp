a521bf7455e4daf4d50375f0728d07d18268f2ba
elephamaximu <elephamaximu@gmail.com>
2018-12-12 17:43:53 +0900

    정인창 : 로그인 서블릿&jsp 초안

--- weatherAnalysis/WebContent/login/login.jsp
+++ weatherAnalysis/WebContent/login/login.jsp
@@ -9,8 +9,17 @@
 </head>
 <body>
 <jsp:include page="/header.jsp"></jsp:include>
-
-<h1> 로그인 페이지 </h1>
-
+<div class="container">
+	<div class="login-form">
+		<h1> 로그인 페이지 </h1>
+		<form action="login.do" method="post">
+			<label for="id">아이디</label>
+			<input type="text" name="id" id="id" required="required"><br>
+			<label for="password">비밀번호</label>
+			<input type="password" name="password" id="password" required="required"><br>
+			<input type="submit" value="로그인">
+		</form>
+	</div>
+</div>
 </body>
 </html>
--- weatherAnalysis/src/com/brain/controller/login/LoginServlet.java
+++ weatherAnalysis/src/com/brain/controller/login/LoginServlet.java
@@ -6,6 +6,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.brain.model.login.LoginService;
 
 
 /**
@@ -21,6 +24,17 @@ public class LoginServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		request.getRequestDispatcher("/login/login.jsp").forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException { 
+		
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		LoginService service = new LoginService();
+		
+		HttpSession session = request.getSession();
+		
 	}
 
 }
